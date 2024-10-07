3e292c7d78dbcca20ad8e194b7810055b8591bce
luigilunardon <l.lunardon@nchain.com>
2024-10-07 12:25:34 +0200

    docs: improved function documentation

--- src/zkscript/fields/fq2_over_2_residue_equal_u.py
+++ src/zkscript/fields/fq2_over_2_residue_equal_u.py
@@ -1,13 +1,14 @@
 from tx_engine import Script
 
 from src.zkscript.fields.fq4 import Fq4
-from src.zkscript.util.utility_scripts import nums_to_script, pick, roll
+from src.zkscript.util.utility_scripts import nums_to_script, pick
 
 
 class Fq2Over2ResidueEqualU(Fq4):
-    r"""F_q^4 = F_q^2[v] / (v^2 - u).
+    """Represents F_q^4 = F_q^2[v] / (v^2 - u).
 
-    Build F_q^4 as quadratic extension of F_q^2 = F_q[u] / (u^2 - NON_RESIDUE) with residue equal to u.
+    This class constructs F_q^4 as a quadratic extension of
+    F_q^2 = F_q[u] / (u^2 - NON_RESIDUE), with residue equal to u.
     """
 
     def square(
@@ -17,7 +18,24 @@ class Fq2Over2ResidueEqualU(Fq4):
         clean_constant: bool | None,
         is_constant_reused: bool | None = None,
     ) -> Script:
-        """Squaring in Fq4 = F_q^2[v] / (v^2 - u)."""
+        """Perform squaring in Fq4 = F_q^2[v] / (v^2 - u).
+
+        Args:
+            take_modulo (bool): Whether to take modulo after the operation.
+            check_constant (bool | None): Whether to check the modulo constant.
+            clean_constant (bool | None): Whether to delete the modulo constant after the operation.
+            is_constant_reused (bool | None, optional): Whether the modulo constant is reused after the current operation.
+
+        Returns:
+            Script: Locking script performing squaring in Fq4 = F_q^2[v] / (v^2 - u).
+
+        """
+
+        # Check the modulo constant
+        # stack in:    [modulo, ..., x0, x1, x2, x3]
+        # altstack in: []
+        # stack out:    [modulo, ..., x0, x1, x2, x3]
+        # altstack out: []
 
         if check_constant:
             out = Script.parse_string("OP_DEPTH OP_1SUB OP_PICK")
@@ -26,9 +44,9 @@ class Fq2Over2ResidueEqualU(Fq4):
         else:
             out = Script()
 
-        # Fourth component ------
-
-        # After this, the stack is: x0 x1 x2 x3, altstack = [2*(x1*x2 + x0*x3)]
+        # Compute the coefficient of uv in (x0 + x1*u + x2*v + x3*uv)^2
+        # stack out:    [..., x0, x1, x2, x3]
+        # altstack out: [uv coefficient := 2*(x1*x2 + x0*x3)]
         out += Script.parse_string("OP_2OVER OP_2OVER")
         out += Script.parse_string("OP_TOALTSTACK")
         out += Script.parse_string("OP_MUL")
@@ -36,9 +54,9 @@ class Fq2Over2ResidueEqualU(Fq4):
         out += Script.parse_string("OP_ADD OP_2 OP_MUL")
         out += Script.parse_string("OP_TOALTSTACK")
 
-        # Third component -------
-
-        # After this, the stack is: x0 x1 x2 x3, altstack = [fourth_component, 2*(x0*x2 + x1*x3*NON_RESIDUE)]
+        # Compute the coefficient of v in (x0 + x1*u + x2*v + x3*uv)^2
+        # stack out:    [..., x0, x1, x2, x3]
+        # altstack out: [uv coefficient, v coefficient := 2*(x0*x2 + x1*x3*NON_RESIDUE)]
         out += Script.parse_string("OP_2OVER OP_2OVER")
         out += Script.parse_string("OP_ROT")
         out += Script.parse_string("OP_MUL")
@@ -48,10 +66,9 @@ class Fq2Over2ResidueEqualU(Fq4):
         out += Script.parse_string("OP_ADD OP_2 OP_MUL")
         out += Script.parse_string("OP_TOALTSTACK")
 
-        # Second component ------
-
-        # After this, the stack is: x0 x1 x2 x3,
-        # altstack = [fourth_component, third_component, 2*x0*x1 + x2^2 + x3^2 * NON_RESIDUE]
+        # Compute the coefficient of u in (x0 + x1*u + x2*v + x3*uv)^2
+        # stack out:    [..., x0, x1, x2, x3]
+        # altstack out: [uv coefficient, v coefficient, u coefficient := 2*x0*x1 + x2^2 + x3^2*NON_RESIDUE]
         out += Script.parse_string("OP_2OVER OP_2OVER")
         out += Script.parse_string("OP_DUP OP_MUL")
         out += nums_to_script([self.BASE_FIELD.NON_RESIDUE])
@@ -64,10 +81,9 @@ class Fq2Over2ResidueEqualU(Fq4):
         out += Script.parse_string("OP_ADD")
         out += Script.parse_string("OP_TOALTSTACK")
 
-        # First component -------
-
-        # After this, the stack is: x0^2 + (x1^2 + 2*x2*x3)*NON_RESIDUE,
-        # altstack = [fourth_component, third_component, second_component]
+        # Compute the zero term in (x0 + x1*u + x2*v + x3*uv)^2
+        # stack out:    [..., x0, x1, x2, x3, zero term := x0^2 + (x1^2 + 2*x2*x3)*NON_RESIDUE]
+        # altstack out: [uv coefficient, v coefficient, u coefficient]
         out += Script.parse_string("OP_2 OP_MUL OP_MUL")
         out += Script.parse_string("OP_SWAP")
         out += Script.parse_string("OP_DUP OP_MUL OP_ADD")
@@ -76,19 +92,24 @@ class Fq2Over2ResidueEqualU(Fq4):
         out += Script.parse_string("OP_SWAP")
         out += Script.parse_string("OP_DUP OP_MUL OP_ADD")
 
+        # Take the modulo of the computed terms or not and move them to the stack
+        # stack out:    [..., x0, x1, x2, x3, zero term, uv coefficient, v coefficient, u coefficient]
+        # altstack out: []
         if take_modulo:
             batched_modulo = Script()
 
             if clean_constant is None and is_constant_reused is None:
-                err = f"If take_modulo is set, both clean_constant: {clean_constant} \
-                        and is_constant_reused: {is_constant_reused} must be set."
-                raise ValueError(err)
+                msg = (
+                    f"If take_modulo is set, both clean_constant: {clean_constant} "
+                    f"and is_constant_reused: {is_constant_reused} must be set."
+                )
+                raise ValueError(msg)
 
-            if clean_constant:
-                fetch_q = Script.parse_string("OP_DEPTH OP_1SUB OP_ROLL")
-            else:
-                fetch_q = Script.parse_string("OP_DEPTH OP_1SUB OP_PICK")
-
+            fetch_q = (
+                Script.parse_string("OP_DEPTH OP_1SUB OP_ROLL")
+                if clean_constant
+                else Script.parse_string("OP_DEPTH OP_1SUB OP_PICK")
+            )
             batched_modulo += Script.parse_string("OP_TUCK OP_MOD OP_OVER OP_ADD OP_OVER OP_MOD")
             batched_modulo += Script.parse_string("OP_FROMALTSTACK OP_ROT")
             batched_modulo += Script.parse_string("OP_TUCK OP_MOD OP_OVER OP_ADD OP_OVER OP_MOD")
@@ -114,8 +135,24 @@ class Fq2Over2ResidueEqualU(Fq4):
         clean_constant: bool | None,
         is_constant_reused: bool | None = None,
     ) -> Script:
-        """Multiplication in Fq4 = F_q^2[v] / (v^2 - u)."""
+        """Perform multiplication in Fq4 = F_q^2[v] / (v^2 - u).
 
+        Args:
+            take_modulo (bool): Whether to take modulo after the operation.
+            check_constant (bool | None): Whether to check the modulo constant.
+            clean_constant (bool | None): Whether to delete the modulo constant after the operation.
+            is_constant_reused (bool | None, optional): Whether the modulo constant is reused after the current operation.
+
+        Returns:
+            Script: Locking script performing multiplication in Fq4 = F_q^2[v] / (v^2 - u).
+
+        """
+
+        # Check the modulo constant
+        # stack in:    [modulo, ..., x0, x1, x2, x3, y0, y1, y2, y3]
+        # altstack in: []
+        # stack out:    [modulo, ..., x0, x1, x2, x3, y0, y1, y2, y3]
+        # altstack out: []
         if check_constant:
             out = Script.parse_string("OP_DEPTH OP_1SUB OP_PICK")
             out += nums_to_script([self.MODULUS])
@@ -123,9 +160,9 @@ class Fq2Over2ResidueEqualU(Fq4):
         else:
             out = Script()
 
-        # Fourth component ------
-
-        # After this, the stack is: x0 x1 x2 x3 y0 y1 y2 y3, altstack = [x0*y3 + x1*y2 + x2*y1 + x3*y0]
+        # Compute the coefficient of uv in (x0 + x1*u + x2*v + x3*uv)*(y0 + y1*u + y2*v + y3*uv)
+        # stack out:    [..., x0, x1, x2, x3, y0, y1, y2, y3]
+        # altstack out: [uv coefficient := x0*y3 + x1*y2 + x2*y1 + x3*y0]
         out += Script.parse_string("OP_2OVER OP_2OVER")
         out += pick(position=11, n_elements=1)
         out += Script.parse_string("OP_MUL OP_ROT")
@@ -137,10 +174,9 @@ class Fq2Over2ResidueEqualU(Fq4):
         out += Script.parse_string("OP_MUL OP_ADD")
         out += Script.parse_string("OP_TOALTSTACK")
 
-        # Third component -------
-
-        # After this, the stack is: x0 x1 x2 x3 y0 y1 y2 y3, altstack = [fourth_component,
-        # x2 * y0 + x0 * y2 + (x1 * y3 + x3 * y1) * NON_RESIDUE]
+        # Compute the coefficient of v in (x0 + x1*u + x2*v + x3*uv)*(y0 + y1*u + y2*v + y3*uv)
+        # stack out:    [..., x0, x1, x2, x3, y0, y1, y2, y3]
+        # altstack out: [uv coefficient, v coefficient := x2*y0 + x0*y2 + (x1*y3 + x3*y1)*NON_RESIDUE]
         out += Script.parse_string("OP_3DUP")
         out += pick(position=9, n_elements=1)
         out += Script.parse_string("OP_MUL OP_ROT")
@@ -155,10 +191,9 @@ class Fq2Over2ResidueEqualU(Fq4):
         out += Script.parse_string("OP_MUL OP_ADD")
         out += Script.parse_string("OP_TOALTSTACK")
 
-        # Second component ------
-
-        # After this, the stack is: x0 x1 x2 x3 y0 y1 y2 y3,
-        # altstack = [fourth_component, third_component, x0 * y1 + x1 * y0 + x2 * y2 + x3 * y3 * NON_RESIDUE]
+        # Compute the coefficient of u in (x0 + x1*u + x2*v + x3*uv)*(y0 + y1*u + y2*v + y3*uv)
+        # stack out:    [..., x0, x1, x2, x3, y0, y1, y2, y3]
+        # altstack out: [uv coefficient, v coefficient, u coefficient := x0*y1 + x1*y0 + x2*y2 + x3*y3*NON_RESIDUE]
         out += Script.parse_string("OP_2OVER OP_2OVER")
         out += pick(position=8, n_elements=1)
         out += Script.parse_string("OP_MUL")
@@ -172,11 +207,9 @@ class Fq2Over2ResidueEqualU(Fq4):
         out += Script.parse_string("OP_MUL OP_ADD")
         out += Script.parse_string("OP_TOALTSTACK")
 
-        # First component -------
-
-        # After this, the stack is: x0 *y0 + (x1 * y1 + x2 * y3 + x3 * y2) * NON_RESIDUE,
-        # altstack = [fourth_component, third_component, second_component]
-
+        # Compute the zero term in (x0 + x1*u + x2*v + x3*uv)*(y0 + y1*u + y2*v + y3*uv)
+        # stack out:    [..., x0, x1, x2, x3, y0, y1, y2, y3, zero term := x0*y0 + (x1*y1 + x2*y3 + x3*y2)*NON_RESIDUE]
+        # altstack out: [uv coefficient, v coefficient, u coefficient]
         out += Script.parse_string("OP_2ROT OP_TOALTSTACK OP_MUL OP_SWAP OP_FROMALTSTACK OP_MUL OP_ADD OP_TOALTSTACK")
         out += Script.parse_string("OP_ROT OP_MUL OP_TOALTSTACK")
         out += Script.parse_string("OP_MUL")
@@ -184,20 +217,24 @@ class Fq2Over2ResidueEqualU(Fq4):
         out += nums_to_script([self.BASE_FIELD.NON_RESIDUE])
         out += Script.parse_string("OP_MUL OP_ADD")
 
+        # Take the modulo of the computed terms or not and move them to the stack
+        # stack out:    [..., x0, x1, x2, x3, zero term, uv coefficient, v coefficient, u coefficient]
+        # altstack out: []
         if take_modulo:
-
             batched_modulo = Script()
 
             if clean_constant is None and is_constant_reused is None:
-                err = f"If take_modulo is set, both clean_constant: {clean_constant} \
-                        and is_constant_reused: {is_constant_reused} must be set."
-                raise ValueError(err)
+                msg = (
+                    f"If take_modulo is set, both clean_constant: {clean_constant} "
+                    f"and is_constant_reused: {is_constant_reused} must be set."
+                )
+                raise ValueError(msg)
 
-            if clean_constant:
-                fetch_q = Script.parse_string("OP_DEPTH OP_1SUB OP_ROLL")
-            else:
-                fetch_q = Script.parse_string("OP_DEPTH OP_1SUB OP_PICK")
-
+            fetch_q = (
+                Script.parse_string("OP_DEPTH OP_1SUB OP_ROLL")
+                if clean_constant
+                else Script.parse_string("OP_DEPTH OP_1SUB OP_PICK")
+            )
             batched_modulo += Script.parse_string("OP_TUCK OP_MOD OP_OVER OP_ADD OP_OVER OP_MOD")
             batched_modulo += Script.parse_string("OP_FROMALTSTACK OP_ROT")
             batched_modulo += Script.parse_string("OP_TUCK OP_MOD OP_OVER OP_ADD OP_OVER OP_MOD")
