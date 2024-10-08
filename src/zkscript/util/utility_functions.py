802c099930fc2f5884885fd73edb5a9a45859743
mducroux <144219013+mducroux@users.noreply.github.com>
2024-10-08 15:04:56 +0200

    fix: typo in optimise_script util function (#18)

--- src/zkscript/util/utility_functions.py
+++ src/zkscript/util/utility_functions.py
@@ -5,8 +5,8 @@ def optimise_script(script: Script) -> Script:
     """Optimise a script by removing redundant operations."""
     patterns = ["OP_TOALTSTACK OP_FROMALTSTACK", "OP_FROMALTSTACK OP_TOALTSTACK"]
 
-    optimise_script = script.to_string()
+    optimised_script = script.to_string()
     for pattern in patterns:
-        optimised_script = optimise_script.replace(pattern, "")
+        optimised_script = optimised_script.replace(pattern, "")
 
     return Script.parse_string(optimised_script)
