245dee2fed6120e8e7077838c2ea5435ca6132c5
Mathieu Ducroux <m.ducroux@nchain.com>
2024-10-09 09:46:45 +0200

    perf: lower optimise_script complexity to O(n) using stack

--- src/zkscript/util/utility_functions.py
+++ src/zkscript/util/utility_functions.py
@@ -2,24 +2,40 @@ from tx_engine import Script
 
 
 def optimise_script(script: Script) -> Script:
-    """Optimize a script by removing redundant operations from and to the altstack.
+    """Optimise a script by removing redundant operations from and to the altstack.
 
     This function removes pairs of redundant operations, such as `OP_TOALTSTACK OP_FROMALTSTACK` and
     `OP_FROMALTSTACK OP_TOALTSTACK`, which cancel each other out. The function iterates over the script
     until no further redundant operations can be removed.
 
     Args:
-        script (Script): The script to be optimized.
+        script (Script): The script to be optimised.
 
     Returns:
-        Script: The optimized script with redundant operations removed.
+        The optimised script with redundant operations removed.
 
     """
-    patterns = ["OP_TOALTSTACK OP_FROMALTSTACK", "OP_FROMALTSTACK OP_TOALTSTACK"]
+    patterns = [
+        ["OP_TOALTSTACK", "OP_FROMALTSTACK"],
+        ["OP_FROMALTSTACK", "OP_TOALTSTACK"],
+        ["OP_ROT", "OP_ROT", "OP_ROT"],
+    ]
 
-    optimised_script = script.to_string()
-    for pattern in patterns:
-        while pattern in optimised_script:
-            optimised_script = optimised_script.replace(pattern, "")
+    script_list = script.to_string().split()
+    stack = []
 
-    return Script.parse_string(optimised_script)
+    for op in script_list:
+        stack.append(op)
+
+        for pattern in patterns:
+            pattern_length = len(pattern)
+
+            if len(stack) >= pattern_length:
+                last_elements = stack[-pattern_length:]
+
+                if last_elements == pattern:
+                    for _ in range(pattern_length):
+                        stack.pop()
+                    break
+
+    return Script.parse_string(" ".join(stack))
