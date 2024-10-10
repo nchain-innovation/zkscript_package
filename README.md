bfdfff21de50b2017545c0b9de4e5f80cadd9ab8
luigilunardon <l.lunardon@nchain.com>
2024-10-10 11:40:50 +0200

    fix: modify optimise_script function to support recursive removal (#21)
    * fix: modify oprimise_script function to support recursive removal
    
    * docs: fix documentation
    
    * perf: lower optimise_script complexity to O(n) using stack
    
    * test: add optimise_script tests
    
    * docs: update README.md
    
    * docs: fixed typos in README.md
    
    ---------
    
    Co-authored-by: Mathieu Ducroux <m.ducroux@nchain.com>

--- README.md
+++ README.md
@@ -48,8 +48,8 @@ The script sizes for Groth16 are:
 
 | Curve | # public statements | Unlocking script size | Locking script size | Modulo threshold | Total |
 | ----- | ------------------- | --------------------- | ------------------- | ---------------- | ----- |
-| `BLS12-381` | 2 | ~ 60 KB | ~ 473 KB | 200B | ~ 533 KB |
-| `MNT4-753` | 1 | ~ 400 KB | ~ 658 KB | 200B | ~ 1.06 MB |
+| `BLS12-381` | 2 | ~ 60 KB | ~ 472 KB | 200B | ~ 532 KB |
+| `MNT4-753` | 1 | ~ 400 KB | ~ 655 KB | 200B | ~ 1.05 MB |
 
 Note: the unlocking script is dependent on the public statements. 
 
--- src/zkscript/util/utility_functions.py
+++ src/zkscript/util/utility_functions.py
@@ -2,11 +2,40 @@ from tx_engine import Script
 
 
 def optimise_script(script: Script) -> Script:
-    """Optimise a script by removing redundant operations."""
-    patterns = ["OP_TOALTSTACK OP_FROMALTSTACK", "OP_FROMALTSTACK OP_TOALTSTACK"]
+    """Optimise a script by removing redundant operations from and to the altstack.
 
-    optimised_script = script.to_string()
-    for pattern in patterns:
-        optimised_script = optimised_script.replace(pattern, "")
+    This function removes pairs of redundant operations, such as `OP_TOALTSTACK OP_FROMALTSTACK` and
+    `OP_FROMALTSTACK OP_TOALTSTACK`, which cancel each other out. The function iterates over the script
+    until no further redundant operations can be removed.
 
-    return Script.parse_string(optimised_script)
+    Args:
+        script (Script): The script to be optimised.
+
+    Returns:
+        The optimised script with redundant operations removed.
+
+    """
+    patterns = [
+        ["OP_TOALTSTACK", "OP_FROMALTSTACK"],
+        ["OP_FROMALTSTACK", "OP_TOALTSTACK"],
+        ["OP_ROT", "OP_ROT", "OP_ROT"],
+    ]
+
+    script_list = script.to_string().split()
+    stack = []
+
+    for op in script_list:
+        stack.append(op)
+
+        for pattern in patterns:
+            pattern_length = len(pattern)
+
+            if len(stack) >= pattern_length:
+                last_elements = stack[-pattern_length:]
+
+                if last_elements == pattern:
+                    for _ in range(pattern_length):
+                        stack.pop()
+                    break
+
+    return Script.parse_string(" ".join(stack))
