4722bfa90f5161a443e5fd11b0d8559612cfa2a2
mducroux-nch <m.ducroux@nchain.com>
2024-10-15 14:55:28 +0200

    docs: add doc for __init__ in bilinear_pairings

---
+++ src/zkscript/bilinear_pairings/__init__.py
@@ -0,0 +1,27 @@
+"""bilinear_pairings package.
+
+This package provides subpackages for constructing Bitcoin scripts that perform bilinear pairings over multiple curves.
+
+Subpackages:
+    - bls12_381: Contains modules for constructing Bitcoin scripts that perform bilinear pairings over BLS12-381.
+    - mnt4_753: Contains modules for constructing Bitcoin scripts that perform bilinear pairings over MNT4-753.
+    - model: Contains modules for ???
+
+Usage example:
+    >>> from src.zkscript.bilinear_pairings.bls12_381.bls12_381 import bls12_381
+    >>>
+    >>> # Script that, taken two points P, Q and some additional data, compute the pairing e(P,Q)
+    >>> bls12_381_pairing = bls12_381.pairing(
+    >>>     modulo_threshold = 1,  # The max size a number is allowed to reach during script execution
+    >>>     check_constant = True,
+    >>>     clean_constant = True,
+    >>> )
+    >>>
+    >>> # Script that, taken two points P1, P2, P3, Q1, Q2, Q3, and some additional data, compute the product of the
+    >>> # three pairings e(P1,Q1) * e(P2,Q2) * e(P3,Q3)
+    >>> bls12_381_triple_pairing = bls12_381.triple_pairing(
+    >>>     modulo_threshold = 1,
+    >>>     check_constant = True,
+    >>>     clean_constant = True,
+    >>> )
+"""
---
+++ src/zkscript/bilinear_pairings/bls12_381/__init__.py
@@ -0,0 +1,12 @@
+"""bls12_381 package.
+
+This package provides modules for constructing Bitcoin scripts for operations specific to BLS12-381.
+
+Modules:
+    - bls12_381: Build pairing model for BLS12-381.
+    - fields: Finite field arithmetic for BLS12-381.
+    - final_exponentiation: Final exponentiation for BLS12-381.
+    - line_functions: Line evaluation for BLS12-381.
+    - miller_output_operations: Operations between Miller output (of type Fq4) and line evaluations.
+    - parameters: BLS12-381 curve parameters.
+"""
