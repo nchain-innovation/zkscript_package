0b9468ac6975d2c148aa079c45b19862af7fcbce
Federico Barbacovi <f.barbacovi@nchain.com>
2024-11-18 09:46:25 +0000

    Fill __init__.py for TransactionIntrospection

---
+++ src/zkscript/transaction_introspection/__init__.py
@@ -0,0 +1,14 @@
+"""transaction_introspection package.
+
+This package provides modules for constructing Bitcoin scripts that achieve transaction introspection.
+
+Modules:
+    - transaction_introspection: Contains the TransactionIntrospection class for scripts that achieve
+        transaction introspection.
+
+Usage example:
+    >>> from tx_engine import SIGHASH
+    >>> from src.zkscript.transaction_introspection.transaction_introspection import TransactionIntrospection
+    >>>
+    >>> pushtx_script = TransactionIntrospection.pushtx(sighash=SIGHASH.ALL)
+"""
