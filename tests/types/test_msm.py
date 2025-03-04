b2deee71d1fa11e44db1196182614f6c02b616ac
Federico Barbacovi <f.barbacovi@teranode.group>
2025-03-04 16:23:34 +0000

    Transaction introspection: if not preimage, then pass as number; minor fixes in docs

--- tests/types/test_msm.py
+++ tests/types/test_msm.py
@@ -5,13 +5,11 @@ from elliptic_curves.fields.prime_field import PrimeField
 from elliptic_curves.models.ec import ShortWeierstrassEllipticCurve
 from elliptic_curves.util.zkscript import (
     multi_scalar_multiplication_with_fixed_bases_gradients,
-    unrolled_multiplication_gradients,
 )
 from tx_engine import Context, Script
 
 from src.zkscript.elliptic_curves.ec_operations_fq import EllipticCurveFq
 from src.zkscript.types.unlocking_keys.msm_with_fixed_bases import MsmWithFixedBasesUnlockingKey
-from src.zkscript.types.unlocking_keys.unrolled_ec_multiplication import EllipticCurveFqUnrolledUnlockingKey
 from src.zkscript.util.utility_scripts import nums_to_script
 
 # Data for Secp256k1
@@ -28,57 +26,6 @@ generator = secp256k1(
     infinity=False,
 )
 test_script = EllipticCurveFq(q=modulus, curve_a=0)
-
-
-@pytest.mark.parametrize(
-    ("P", "a", "max_multiplier", "expected"),
-    [
-        (
-            generator,
-            3,
-            8,
-            3,
-        ),
-        (
-            generator,
-            89179908133058966563943425115874784614382846700656822879827803812293592024531,
-            order,
-            89179908133058966563943425115874784614382846700656822879827803812293592024531,
-        ),
-        (
-            generator,
-            99918161303508978633620523839997829444876269911501350624465443203877613350,
-            2**250,
-            99918161303508978633620523839997829444876269911501350624465443203877613350,
-        ),
-        (
-            generator,
-            245130299858301666475531021987374198395,
-            2**128,
-            245130299858301666475531021987374198395,
-        ),
-        (
-            generator,
-            10204030,
-            2**128,
-            10204030,
-        ),
-    ],
-)
-def test_extract_scalar_as_unsigned(P, a, expected, max_multiplier):  # noqa: N803
-    unlocking_key = EllipticCurveFqUnrolledUnlockingKey(
-        P=P.to_list(), a=a, gradients=unrolled_multiplication_gradients(a, P).as_data(), max_multiplier=max_multiplier
-    )
-
-    script = unlocking_key.to_unlocking_script(test_script, fixed_length_unlock=True, load_modulus=True)
-    script += unlocking_key.extract_scalar_as_unsigned(rolling_option=True)
-    script += nums_to_script([expected]) + Script.parse_string("OP_EQUALVERIFY")
-    script += Script.parse_string(" ".join(["OP_DROP"] * (int(log2(max_multiplier)) * 2 + 4)))
-    script += Script.parse_string("OP_1")
-
-    context = Context(script=script)
-    assert context.evaluate()
-    assert context.get_stack().size() == 1
 
 
 @pytest.mark.parametrize("index", [0, 1, 2])
